e857e2e14f005c157e46b6992d91f4b6422d6c9a
z0tedd <z0tedd@gmail.com>
2025-05-20 21:19:53 +0300

    build: linux, status: failed

---
+++ mutliline_turing_machine/my_app/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/opt/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/z0tedd/Documents/projects/Multiline-Turing-Machine/mutliline_turing_machine/my_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/opt/flutter"
+  "PROJECT_DIR=/home/z0tedd/Documents/projects/Multiline-Turing-Machine/mutliline_turing_machine/my_app"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzEuMC0xLjAucHJlLjQyMQ==,RkxVVFRFUl9DSEFOTkVMPW1hc3Rlcg==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MjEyMDY0YTNlNQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MjEyMDY0YTNlNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjAgKGJ1aWxkIDMuOC4wLTI2NS4wLmRldik="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/z0tedd/Documents/projects/Multiline-Turing-Machine/mutliline_turing_machine/my_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/z0tedd/Documents/projects/Multiline-Turing-Machine/mutliline_turing_machine/my_app/lib/main.dart"
+)
